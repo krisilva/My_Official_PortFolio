6226dd183be4967ebe847382293ed47d06f98b22
K.Rumesha Ishini Silva <ishinisilva19@gmail.com>
2026-01-28 12:27:04 +0530

    added new photo

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -206,7 +206,7 @@ export default function Hero() {
               {/* Image container with mask */}
               <div className="hero-image-mask relative w-72 h-72 sm:w-96 sm:h-96 lg:w-[500px] lg:h-[500px] rounded-2xl overflow-hidden">
                 <img
-                  src="/001.png"
+                  src="/011.jpeg"
                   alt="Kavirathna Silva"
                   className="w-120 h-200 object-cover"
                 />
